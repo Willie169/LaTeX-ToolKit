194f97762a6dbcc136fb8e07aa80b8fcb7a6fea1
Willie169 <cg10250207@gmail.com>
2025-01-12 15:22:41 +0800

    ma

--- alphabet.c
+++ alphabet.c
@@ -6,7 +6,7 @@ int main() {
     for (char c = 'A'; c <= 'Z'; c++) array[i++] = c;
     for (char c = 'a'; c <= 'z'; c++) array[i++] = c;
     for (int j = 0; j < 52; j++) {
-        printf("\\ProvideDocumentCommand{\\x%c}{}{\\text{%c}}\n\\ProvideDocumentCommand{\\rm%c}{}{\\relax\\ifmmode\\mathrm{%c}\\else\\text{\\(\\mathrm{%c}\\)}\\fi}\n", array[j], array[j], array[j], array[j], array[j]);
-        for (int k = 0; k < 52; k++) printf("\\ProvideDocumentCommand{\\x%c%c}{}{\\text{%c%c}}\n\\ProvideDocumentCommand{\\rm%c%c}{}{\\relax\\ifmmode\\mathrm{%c%c}\\else\\text{\\(\\mathrm{%c%c}\\)}\\fi}\n", array[j], array[k], array[j], array[k], array[j], array[k], array[j], array[k], array[j], array[k]);
+        printf("\\ProvideDocumentCommand{\\tx%c}{}{\\text{%c}}\n\\ProvideDocumentCommand{\\rm%c}{}{\\relax\\ifmmode\\mathrm{%c}\\else\\text{\\(\\mathrm{%c}\\)}\\fi}\n", array[j], array[j], array[j], array[j], array[j]);
+        for (int k = 0; k < 52; k++) printf("\\ProvideDocumentCommand{\\tx%c%c}{}{\\text{%c%c}}\n\\ProvideDocumentCommand{\\rm%c%c}{}{\\relax\\ifmmode\\mathrm{%c%c}\\else\\text{\\(\\mathrm{%c%c}\\)}\\fi}\n", array[j], array[k], array[j], array[k], array[j], array[k], array[j], array[k], array[j], array[k]);
     }
 }
